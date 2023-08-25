cc178206c0e3812dd0a5981c5e34bf721f324f34
RenanRSilva <renan.rambul@takeandgoapp.com>
2023-08-24 23:09:12 -0300

    feat: update user

--- lib/banana_bank_web/controllers/users_json.ex
+++ lib/banana_bank_web/controllers/users_json.ex
@@ -9,6 +9,7 @@ defmodule BananaBankWeb.UsersJSON do
   end
 
   def get(%{user: user}), do: %{data: data(user)}
+  def update(%{user: user}), do: %{message: "User atualizado com sucesso!", data: data(user)}
 
   defp data(%User{} = user) do
     %{
