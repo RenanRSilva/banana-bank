3b67d4de7864c185b57171e748d8759cfa97d2cf
RenanRSilva <renan.rambul@takeandgoapp.com>
2023-08-27 23:59:25 -0300

    test: create user

--- config/test.exs
+++ config/test.exs
@@ -6,10 +6,11 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :banana_bank, BananaBank.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "myuser",
+  password: "myuser",
   hostname: "localhost",
-  database: "banana_bank_test#{System.get_env("MIX_TEST_PARTITION")}",
+  database: "banco",
+  stacktrace: true,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
