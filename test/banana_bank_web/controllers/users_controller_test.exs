f1334412aeac2c18a7e1f9d41ec69d6bc00ee2dc
RenanRSilva <renan.rambul@takeandgoapp.com>
2023-08-28 23:18:18 -0300

    test: error validation in create user

--- test/banana_bank_web/controllers/users_controller_test.exs
+++ test/banana_bank_web/controllers/users_controller_test.exs
@@ -19,5 +19,21 @@ defmodule BananaBankWeb.UsersControllerTest do
 
         assert %{"data" => %{"cep" => "12345678", "email" => "joao@frutas.com", "id" => _id, "name" => "João"}, "message" => "Usuário criado com sucesso!"} = response
     end
+
+    test "when there are invalid params, returns an error", %{conn: conn} do
+      params = %{
+        name: nil,
+        cep: "12",
+        email: "joao@frutas.com",
+        password: "1"
+      }
+
+      response =
+        conn
+        |> post(~p"/api/users", params)
+        |> json_response(:bad_request)
+
+        assert %{"errors" => %{"cep" => ["should be 8 character(s)"]}} = response
+    end
   end
 end
